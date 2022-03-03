791a4f57e6448567a170f12c7b3909f6a8b4fffb
Hain Thit <hainlthit@gmail.com>
2022-03-02 23:48:57 -0500

    postgres! + index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+        render json: users, status: :ok
+    end
+
     def create 
         user = User.create!(user_params)
         render json: user, status: :created
@@ -8,7 +13,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:name, :password)
+        params.permit(:username, :password)
     end 
 
 end
