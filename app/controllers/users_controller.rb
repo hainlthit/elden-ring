d9c56e4bd575fa38bf3a4b8afb1f200265768ed7
Ilolo Izu <iloloizu97@gmail.com>
2022-03-02 18:17:34 -0600

    postgres issues, pls help RIP

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+    def create 
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end 
+
+    private
+
+    def user_params
+        params.permit(:name, :password)
+    end 
+
+end
