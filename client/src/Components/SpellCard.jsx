420107002c721dc237be5acbb57b92d917c3f81a
Ilolo Izu <iloloizu97@gmail.com>
2022-03-04 17:23:38 -0600

    hard coded one test spell

--- client/src/Components/SpellCard.jsx
+++ client/src/Components/SpellCard.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function SpellCard() {
   return (
-    <div>MAP out my contents</div>
+    <>
+      <div className="card">
+        <div className="blog-content">
+            <a href={"https://eldenring.wiki.fextralife.com/Death+Lightning"} className="spell-link">
+                <img src={"https://eldenring.wiki.fextralife.com/file/Elden-Ring/death_lightning_incantation-icon_elden_ring_wiki_guide_200px.png"} alt="blog-image"/>
+                    <p>DEATH LIGHTNING</p> 
+                </a> <br /> 
+            </div>
+         </div>  
+    </>
   )
 }
