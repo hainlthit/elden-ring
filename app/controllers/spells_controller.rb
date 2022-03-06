d39a365dc4321112270c2a85aa3e50365e79d775
Ilolo Izu <iloloizu97@gmail.com>
2022-03-06 16:49:40 -0600

    CORS

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,5 +1,5 @@
 class SpellsController < ApplicationController
-    skip_before_action :authorized_user, only: [:index, :show]
+    # skip_before_action :authorized_user, only: [:index, :show]
     def index
         spells = Spell.all
         render json: spells, status: :ok
--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,16 @@ module ReactRailsApiProjectTemplate
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.api_only = true
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: '*',
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
